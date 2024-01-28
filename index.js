92a441942e62be0640cce3ea860e4c2b2d78b74c
vicky acharjee <vickyacharjeevlg@gmail.com>
2024-01-28 16:51:08 +0530

    initial commit:Added. file

---
+++ index.js
@@ -0,0 +1,40 @@
+const express= require('express');
+const PORT= 80;
+
+const app=express();
+const users=require('./MOCK_DATA.json');
+
+//GET
+//For Server Side Rendering
+app.get('/users',(req,res)=>{
+    // res.send('Hello From Home Page');
+    //return res.json(users);
+    const html=`
+    <ul>
+      ${users.map(users=>`
+      <li><h3>The user id is: <b> ${users.id}</b></h3></li> 
+      <li> ${users.first_name}</li>
+      <li> ${users.gender}</li>
+      <li> ${users.Work_Title}</li> <br>
+      `).join("")}
+    </ul>`;
+    res.send(html);
+})
+
+//For JSON API
+app.get('/api/users',(req,res)=>{
+    res.json(users);
+})
+
+//Lis the specific id Ex: 1,2,3 
+app.get('/api/users/:id',(req,res)=>{
+    const id=Number(req.params.id)
+    const user=users.find((user)=>user.id==id)
+    
+    return res.send(user);
+})
+
+
+app.listen(PORT,()=>console.log(`Server started at ${PORT}`))
+
+
