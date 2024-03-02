414f2d52e53350174726339b4f9347eb38e0b8b2
vicky acharjee <vickyacharjeevlg@gmail.com>
2024-03-02 22:26:01 +0530

    added setHeader, statusCode

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const app=express();
 const PORT=8000;
 const users=require('./mockData.json');
 const fs=require('fs');
+const { error } = require('console');
 
-//MiddleWare plugins
+//MiddleWare plugins for PostMan 
 app.use(express.urlencoded({ extended: false }));
 
 
@@ -22,6 +23,7 @@ app.use((req,res,next)=>{
 })
 
 //Middleware 3
+//for pathName, http method,Dat() 
 app.use((req,res,next)=>{
   fs.appendFile('logForMiddleWare.txt',`\n The method is:${req.method} The path is: ${req.path} Date is: ${Date.now().toLocaleString()}`,(err,data)=>{
     next();
@@ -30,7 +32,8 @@ app.use((req,res,next)=>{
 
 //Home path
 app.get('/',(req,res)=>{
-  res.setHeader("X-Name","vicky acharjee");//custon http header using setHeader
+  // header for additional information
+  res.setHeader("X-Name","vicky acharjee");//custon http header using setHeader()
   res.send("Hi this is home page");
 })
 
@@ -49,19 +52,28 @@ app.get('/api/isers/:id',(req,res)=>{
   
   const id=Number(req.params.id);
   const user=users.find((user)=>user.id===id);
+  if(!user){
+     res.status(404).json({status:"user not found or out of scope"})//validation if it went out of scope since the current limit is 1000+
+  }
   return res.json(user)
 })
 
-//using post method
 
+//using post method
 app.post('/api/users', (req, res) => {
   const body = req.body;
+  if(!body.first_name || !body.last_name || !body.email || !body.gender || !body.Work_Title){ //basic validation 400 indicates bad request  
+    res.status(400).json({status:"Kindly fill the form completely"});
+  }
   users.push({...body, id: users.length+1});
   fs.writeFile('./mockData.json',JSON.stringify(users),(err,data)=>{
-    return res.json({ status: 'send' }); // Assuming 'pending' is a string
+    //adding custom http status code 201 saying created.
+    return res.status(201).json({ status: 'send' }); // Assuming 'pending' is a string
   })
   //console.log("body", body); // Use a comma instead of concatenating directly
 });
+
+
 
 //using routing approach
 app.route('/api/users')
